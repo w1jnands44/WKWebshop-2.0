3b6cbe06cd34cc11a986cc2409119fb7d0c030a7
Wouter Wijnands <wouter-wijnands@live.nl>
2013-07-08 00:04:08 +0200

    Made starting files and basic website structure

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>
+			Homepage - WKWebshop
+		</title>
+		<link href="style/style.css" rel="stylesheet" type="text/css">
+	</head>
+	
+	<body>
+		<div id="prime_holder">
+			<div id="header">
+			</div>
+			<div id="content">
+			</div>
+			<div id="footer">
+			</div>
+		</div>
+	</body>
+</html>
