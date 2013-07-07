31866dc160717fe41bea164cdc5b7fc7abf11329
Wouter Wijnands <wouter-wijnands@live.nl>
2013-07-08 00:54:11 +0200

    Added menu and logo

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
 	<body>
 		<div id="prime_holder">
 			<div id="header">
+				<img src="images/layout/header/wkwebshop.png"/>
+				<div id="menu">
+					<nav>
+						<ul>
+							<a href="/laptops"><li class="button">Laptops</li></a>
+							<a href="/desktops-randapparatuur"><li class="button">Desktops en Randapparatuur</li></a>
+							<a href="/componenten"><li class="button">Componenten</li></a>
+							<a href="/opslag"><li class="button">Opslag</li></a>
+							<a href="/tablets-e-readers"><li class="button">Tablets & E-Readers</li></a>
+							<a href="/gaming-software"><li class="button">Gaming & Software</li></a>
+							<a href="/merken"><li class="button">Merken</li></a>
+						</ul>
+					</nav>
+				</div>
 			</div>
 			<div id="content">
 			</div>
